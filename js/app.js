7af40378c714adc083f9a48a66ae0e36c1358650
A.Adefolalu B <adefolalucode101@gmail.com>
2022-10-15 15:33:28 +0100

    add question and answer for user communication

--- js/app.js
+++ js/app.js
@@ -3,3 +3,39 @@ $(document).ready(function(){
     $()
 
 });
+
+// business logic
+const quizData = [
+    {
+        question: "What country has the highest life expectancy?",
+        a: "Brazil",
+        b: "canada",
+        c: "Japan",
+        d: "Hongkong",
+        correct: "d",
+    },
+    {
+        question: "Which language has the more native speakers?",
+        a: "Arabic",
+        b: "English",
+        c: "Spanish",
+        d: "Yoruba",
+        correct: "c",
+    },
+    {
+        question: "What is the most common surname in the United States?",
+        a: "Smith",
+        b: "Arnold",
+        c: "James",
+        d: "Johnson",
+        correct: "a",
+    },
+    {
+        question: "Who was the Ancient Greek God of the Sun?",
+        a: "Zeus",
+        b: "Kratos",
+        c: "Spartacus",
+        d: "Apollo",
+        correct: "d",
+    },
+];
